c9ffd48b88e098f4ab4cc989308025ac7b474cfe
Karan <duakaran96@gmail.com>
2020-05-04 21:55:15 +0530

    Create Number Complement.cpp
    https://leetcode.com/explore/challenge/card/may-leetcoding-challenge/534/week-1-may-1st-may-7th/3319/

---
+++ Solutions/Week 1: May 1st–May 7th/Number Complement.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int findComplement(int num) {
+        int result = 0, power = 1;
+        while (num > 0) {
+            result += (num % 2 ^ 1) * power;
+            power <<= 1;
+            num >>= 1;
+        }
+        return result;
+    }
+};
